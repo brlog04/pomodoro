b2e93c5338ef8ad351c4f6a05dd82c083b3af13e
Darko <darko.rancic@badin.rs>
2024-09-03 15:51:51 +0200

    dodat breeze

--- routes/web.php
+++ routes/web.php
@@ -1,33 +1,20 @@
 <?php
 
-use App\Http\Controllers\ClientController;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\PomodoreController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\ProjectController;
-use App\Http\Controllers\StatusController;
-use App\Http\Controllers\TaskController;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\UserProfileController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect('home');
+    return view('welcome');
 });
 
-Route::get('/home', [HomeController::class,'index'])->name('home');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::resource('/statuses', StatusController::class);
-Route::resource('/clients', ClientController::class);
-Route::resource('/projects', ProjectController::class);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::resource('tasks', TaskController::class);
-Route::resource('pomodoros', PomodoreController::class);
-Route::resource('userProfiles', UserProfileController::class);
-Route::resource('users', UserController::class);
-
-/*Route::get('pomodoro-test', function(){
-    return view('testpomodoro');
-});*/
-
-Route::get('product',[ProductController::class,'index']);
+require __DIR__.'/auth.php';
